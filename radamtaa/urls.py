94b370ab79b5586a62b724fc19ddc9f79d2aa210
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-10-31 11:57:10 +0300

    This will add the footer

--- radamtaa/urls.py
+++ radamtaa/urls.py
@@ -4,7 +4,11 @@ from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('landing/', views.LandingView.as_view() , name="landing"),
     ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
